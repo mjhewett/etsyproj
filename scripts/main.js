480101fdcd23bc2e0b3ddf8f87e243e9702a5f9d
Max Hewett <mjhewett01@gmail.com>
2015-07-14 12:34:27 -0400

    final commit. fixed stuff.

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,16 @@ var whiskTitle, whiskImage, whiskSeller, whiskPrice, whiskPost, whiskFavorite;
 
 
 javItems.results.forEach( function(item){
-	
-	whiskTitle = '<p class="title"><a href="' + item.url + '">' + item.title + '</a></p>';
-	whiskImage = '<a href="' + item.url + '"><img src="' + item.Images[0].url_170x135 + '"></a>';
-	whiskFavorite= '<a href="#" class="pop-up">' + '<div class="heart"><img src="images/heart.png"></div>' +
-                   '<div class="hamburger"><img src="images/hamburger.png"></div>' + '</a>';
-    whiskSeller ='<p class="seller"><a href="' + item.Shop.url + '">' + item.Shop.shop_name + '</a></p>';
-    whiskPrice ='<p class="price">' + '$' + item.price + ' ' + 'USD' + '</p>';
-    whiskPost = '<li>' + whiskImage + whiskFavorite + whiskTitle + whiskSeller + whiskPrice + '</li>';
 
-    $('.imgCard').append(whiskPost);  
+	whiskTitle = '<p class="title"><a class="titles" href="' + item.url + '">' + item.title + '</a></p>';
+	whiskImage = '<a href="' + item.url + '"><img class="picZo" src="' + item.Images[0].url_170x135 + '"></a>';
+	whiskFavorite= '<a href="#" class="pop-up">' + '<div class="heart"><img class="hearts" src="images/heart.png"></div>' +
+                   '<div class="hamburger"><img class="burger" src="images/hamburger.png"></div>' + '</a>';
+    whiskSeller ='<p class="seller"><a href="' + item.Shop.url + '">' + item.Shop.shop_name + '</a></p>';
+    whiskPrice ='<p class="prices">' + '$' + item.price + ' ' + 'USD' + '</p>';
+    whiskPost = '<li class="imgList">' + whiskImage + whiskFavorite + whiskTitle + whiskSeller + whiskPrice + '</li>';
+
+    $('.imgCard').append(whiskPost);
 
 });
 
