02b30326c31ba178be9129e0f00489799f554aed
Max Hewett <mjhewett01@gmail.com>
2015-10-07 14:46:25 -0400

    readme + site

--- Readme.md
+++ Readme.md
@@ -3,3 +3,5 @@
 >The purpose of this assignment was to recreate an Etsy page by pulling data from the Etsy API using JQuery. The data was formatted on the page using Bourbon and Neat.
 
 > Deployed page: http://mjhewett.github.io/etsyproj/
+
+Run node-sass -w styles/main.scss styles/main.css
--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,17 @@ var whiskTitle, whiskImage, whiskSeller, whiskPrice, whiskPost, whiskFavorite;
 
 javItems.results.forEach( function(item){
 
-	whiskTitle = '<p class="title"><a class="titles" href="' + item.url + '">' + item.title + '</a></p>';
-	whiskImage = '<a href="' + item.url + '"><img class="picZo" src="' + item.Images[0].url_170x135 + '"></a>';
-	whiskFavorite= '<a href="#" class="pop-up">' + '<div class="heart"><img class="hearts" src="images/heart.png"></div>' +
-                   '<div class="hamburger"><img class="burger" src="images/hamburger.png"></div>' + '</a>';
-    whiskSeller ='<p class="seller"><a href="' + item.Shop.url + '">' + item.Shop.shop_name + '</a></p>';
-    whiskPrice ='<p class="prices">' + '$' + item.price + ' ' + 'USD' + '</p>';
-    whiskPost = '<li class="imgList">' + whiskImage + whiskFavorite + whiskTitle + whiskSeller + whiskPrice + '</li>';
+    	whiskTitle = '<p class="title"><a class="titles" href="' + item.url + '">' + item.title + '</a></p>';
+    	whiskImage = '<a href="' + item.url + '"><img class="picZo" src="' + item.Images[0].url_170x135 + '"></a>';
+    	whiskFavorite= '<a href="#" class="pop-up">' + '<div class="heart"><img class="hearts" src="images/heart.png"></div>' +
+                      '<div class="hamburger"><img class="burger" src="images/hamburger.png"></div>' + '</a>';
+      whiskSeller ='<p class="seller"><a href="' + item.Shop.url + '">' + item.Shop.shop_name + '</a></p>';
+      whiskPrice ='<p class="prices">' + '$' + item.price + ' ' + 'USD' + '</p>';
+      whiskPost = '<li class="imgList">' + whiskImage + whiskFavorite + whiskTitle + whiskSeller + whiskPrice + '</li>';
 
     $('.imgCard').append(whiskPost);
+  });
 
-});
 
 
 
